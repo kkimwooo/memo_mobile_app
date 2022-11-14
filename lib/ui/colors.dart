cdd1e32eac6fd419921a72728ac55e7997182fde
kkimwooo <kkimwooo@gmail.com>
2022-11-14 19:36:08 +0900

    color.dart Color 정의

---
+++ lib/ui/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const yellow = Colors.yellow;
+const red = Colors.red;
+const orange = Colors.orange;
+const green = Colors.green;
+const blue = Colors.blue;
+
+const grey = Colors.grey;
+const lightBlue = Colors.lightBlue;
